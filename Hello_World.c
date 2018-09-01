e18f45fd166f5390c643a38a9a8b8a6cb3ff249d
Prasad Ashok Dukale <prasad.dukale@atos.net>
2018-09-01 12:31:49 +0530

    Create Hello_World.c

---
+++ Hello_World.c
@@ -0,0 +1,17 @@
+// domentation
+
+
+// preprocessor directives
+#include <stdio.h>
+
+// global variable declaration
+
+// main method
+int main()
+{
+    printf("Hello, World!\n");
+
+    return 0;
+}
+
+//user difined methods
