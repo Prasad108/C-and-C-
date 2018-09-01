28259cee297537cb2b21c05b69bed3fc02be43e0
Prasad Ashok Dukale <prasad.dukale@atos.net>
2018-09-01 12:36:41 +0530

    Structure Of the C Programm

--- Hello_World.c
+++ Hello_World.c
@@ -1,17 +1,28 @@
-// domentation
+/** domentation
+ * Author Prasad Ashok Dukale
+ * 
+ */
 
 
 // preprocessor directives
 #include <stdio.h>
 
 // global variable declaration
+int K=10;
 
 // main method
 int main()
 {
     printf("Hello, World!\n");
 
+    printf("Global variable is %d \n",K);
+    
+    userDifienedFunction();
     return 0;
 }
 
 //user difined methods
+
+void userDifienedFunction(){
+    printf("Hello World from the Function");
+}
